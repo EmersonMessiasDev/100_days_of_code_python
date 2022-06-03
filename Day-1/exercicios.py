add7b3d23a824e973ecee0b7db8ff7b62bd3265d
emerson <emerson.almeida@a.ficr.com.br>
2022-06-03 20:08:40 -0300

    100_days_of_code_python

---
+++ Day-1/exercicios.py
@@ -0,0 +1,21 @@
+
+#?Instruções
+"""Escreva um programa que some os algarismos de um número de 2 algarismos. por exemplo, se a entrada foi 35, então a saída deve ser 3 + 5 = 8
+
+Aviso. Não altere o código nas linhas 1-3. Seu programa deve funcionar para diferentes entradas. por exemplo, qualquer número de dois dígitos."""
+
+
+#! 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+#! 🚨 Don't change the code above 👆
+
+#?###################################
+#?Write your code below this line 👇
+print(int(two_digit_number[0]) + int(two_digit_number[1]))
+
+# if len(two_digit_number) == 2:
+#     result = int(two_digit_number[0]) + int(two_digit_number[1])
+#     print(result)
+# else:
+#     pass
+
